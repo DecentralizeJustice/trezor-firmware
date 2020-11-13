f57aca2f2a1dc5974ab0e3fb5766313dae71e197
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-11-13 16:29:53 -0500

    build: added notes and version check

--- python/build_bin.sh
+++ python/build_bin.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
+# gen notes: make sure penv and pyinstaller are working
 # Script for building standalone binary
 
 rm -r dist build
-#env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.6.9
+env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.6.9
 pyenv local 3.6.9
 pip install https://github.com/pyinstaller/pyinstaller/tarball/develop
 
 python setup.py install
 
-
+pyinstaller --version
 pyinstaller trezorCliTool.spec
