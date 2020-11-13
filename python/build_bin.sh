68a0a1b3cda30a7f52dbbb8b0d7d9e7397d07dfb
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2020-11-13 16:27:23 -0500

    biary: updated python version

--- python/build_bin.sh
+++ python/build_bin.sh
@@ -2,11 +2,11 @@
 # Script for building standalone binary
 
 rm -r dist build
-# env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.6.8
-pyenv local 3.6.8
-#pip install https://github.com/pyinstaller/pyinstaller/tarball/develop
+#env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.6.9
+pyenv local 3.6.9
+pip install https://github.com/pyinstaller/pyinstaller/tarball/develop
 
-# python setup.py install
+python setup.py install
 
 
 pyinstaller trezorCliTool.spec
