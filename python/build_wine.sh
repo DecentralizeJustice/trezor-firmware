95054ad14c07621cfaf0e16e85b8c9869a74c3d7
itsMikeLowrey <32291941+itsMikeLowrey@users.noreply.github.com>
2020-11-20 10:57:30 -0500

    ls

--- python/build_wine.sh
+++ python/build_wine.sh
@@ -79,14 +79,14 @@ sleep 5 # For some reason, pausing for a few seconds makes the next step work
 #     sed -i 's/raise()/raise_()/g' $gen_file
 # done
 # popd
-
-# Do the build
-export PYTHONHASHSEED=42
-$POETRY run pyinstaller trezorCliTool.spec
-unset PYTHONHASHSEED
-
-# Make the final compressed package
-pushd dist
-VERSION=`$POETRY run hwi --version | cut -d " " -f 2 | dos2unix`
-zip "hwi-${VERSION}-windows-amd64.zip" trezorCliTool.exe
-popd
+ls
+# # Do the build
+# export PYTHONHASHSEED=42
+# $POETRY run pyinstaller trezorCliTool.spec
+# unset PYTHONHASHSEED
+#
+# # Make the final compressed package
+# pushd dist
+# VERSION=`$POETRY run hwi --version | cut -d " " -f 2 | dos2unix`
+# zip "hwi-${VERSION}-windows-amd64.zip" trezorCliTool.exe
+# popd
