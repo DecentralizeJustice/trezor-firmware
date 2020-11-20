309fab3de6bde44a42573a5d3ac922bc54be7124
itsMikeLowrey <32291941+itsMikeLowrey@users.noreply.github.com>
2020-11-20 12:03:25 -0500

    no fault

--- python/build_bin.sh
+++ python/build_bin.sh
@@ -2,7 +2,7 @@
 # gen notes: make sure penv and pyinstaller are working
 # Script for building standalone binary
 
-rm -r dist build
+#rm -r dist build
 env PYTHON_CONFIGURE_OPTS="--enable-framework" pyenv install 3.6.9
 pyenv local 3.6.9
 pip install https://github.com/pyinstaller/pyinstaller/tarball/develop
